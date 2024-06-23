30b52d24e91f24eff6ee26f4194d55ee759accc1
hubofyashraj <yashraj.jangir69@gmail.com>
2024-06-23 19:40:39 +0530

    .

--- backend/src/routes/upload.ts
+++
@@ -1,9 +0,0 @@
-import multer from 'multer'
-
-const postStorage = multer.memoryStorage();
-
-export const postUploadMiddleware = multer({storage: postStorage})
-
-const pfpStorage = multer.memoryStorage();
-
-export const pfpUploadMiddleware = multer({storage: pfpStorage})
